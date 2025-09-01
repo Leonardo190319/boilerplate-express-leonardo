4c29d8d2a998436e721b68ca255f83c111ae3eca
Leonardo <leonardolindob494@gmail.com>
2025-08-31 21:44:07 -0500

    Add MESSAGE_STYLE env handling for /json

--- myApp.js
+++ myApp.js
@@ -1,23 +1,14 @@
-require('dotenv').config();
+const express = require("express");
+const app = express();
 
-var express = require('express');
-var app = express();
-
-
-app.use("/public", express.static(__dirname + "/public"));
-
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-
-app.get("/json", function(req, res) {
+app.get("/json", (req, res) => {
   let message = "Hello json";
+
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
   }
-  res.json({ "message": message });
+
+  res.json({ message: message });
 });
 
 module.exports = app;
